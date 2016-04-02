0ff7f5ace8194c8a5a4092f87ea6a4b930262241
sunbather <mjplattner@gmail.com>
2016-04-02 12:37:20 -0400

    removed the global variables and tweaked the way the tasks were being stored / spit back out

--- lib/to_do_list.rb
+++ lib/to_do_list.rb
@@ -1,17 +1,37 @@
+require 'pry'
+
 class List
   attr_accessor :name
 
   def initialize(name)
-    @tasks_created = 0
     @name = name
     @task_list = {}
     @all_tasks = []
     @i = 0
   end
 
-  # def instance_counter
-  #   @i += 1
-  # end
+  def counter
+    @i += 1
+  end
+
+  def create_new_task(title, description)
+    tasker = Task.new(title,description)
+    @all_tasks.push(instance_variable_set("@task#{@i}", tasker))
+    counter
+    # this creates and saves the instance of the task / assigns a variable
+  end
+
+  def puts_tasks
+    # clears the key and values of a hash because
+    # each time we want to generate this new because if i change the title
+    # of a task it will create a new k => v pair instead of replacing
+    @task_list.clear
+    @all_tasks.each do |x|
+      @task_list[x.title] = x.detail
+    end
+    puts "Your list #{@name}"
+    @task_list.each {|k, v| puts "#{k} >> #{v}"}
+  end
 
   def tasker_questions(num)
     num.each do |x|
@@ -22,37 +42,16 @@ class List
       create_new_task(name, details)
     end
   end
-
- def your_to_do
-   puts "your list #{@name}:"
-   @task_list.each {|k, v| puts "#{k} - #{v}"}
- end
-
-  def create_new_task(title, description)
-    tasker = Task.new(title,description)
-    tasker.store_this
-    @task_list[tasker.title] = tasker.detail
-    #instance_counter
-  end
-
 end
 
 class Task
-@@instances = []
+
 attr_accessor :title, :detail, :state
 
 def initialize(title, detail)
   @state = "need to do"
   @title = title
   @detail = detail
-end
-
-def self.instances
-  @@instances
-end
-
-def store_this
-  @@instances << self
 end
 
 end
@@ -80,26 +79,29 @@ number_o_tasks = gets.chomp.to_i
 if number_o_tasks <= 0
   puts "plz use human numbers or GTFO"
 end
+# store and name instance variables for each one
+
 task_aray = Array.new(number_o_tasks)
 user_list.tasker_questions(task_aray)
 
-user_list.your_to_do
 
-Task.instances.each do |x|
-  print x.title + " "
-  print x.detail + " "
-  print x.state
-  puts
-end
+user_list.puts_tasks
+
+
 
 
 ## >> ## ^^ TO DO FOR THIS (LOL) ^^ ## << ##
-# so the instance of the task class kind of just gets destoryed :/
-# added a global variable
-# this is bad news
-# i think I should store the key value pair as a hash in an ordered array
-# that way i can access the instance of the task class by calling
-# Task.instance[0] based on the order the task is added to the class
-# its not great but whatevs
+# was able to remove the class Task variable by storing the instances
+# created in my List class in an array
 
-## need to decouple tasker questions 
+## need to decouple tasker questions
+## put together a lil method to check if a task exists and if so change any of that tasks values
+## already kind of have that in a quick and dirty test file
+## rename the instance variables and method names to not be so dumb
+
+
+### i had a hard time with where to start with the tracking instance problem
+### i think a big part of that problem was that I was trying to tackle the issue
+### in the context of the entire project instead of thinking of it in a more abstracted way
+### i ended up just writing a really simple two class thing in a throw away file
+### to figure out and quickly test how it should work.
